af7edb6f6571d332a08418e383bc7459d05db42c
César Damasceno <64749161+ManeMaria@users.noreply.github.com>
2020-06-26 16:44:53 -0300

    inital commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Section1 from "./Section1.js";
+import Section2 from "./Section2.js";
+import Section3 from "./Section3.js";
+import Footer from "./Footer.js"
+import img from './assets/img/logo-cd-p.png';
+
+export default class App extends React.Component{
+
+    render(){
+        const conteudoTxt = {
+            texth1: "E -BOOK" ,
+            texth2: "A MORALIDADE DO CAPITALISMO"
+        }
+        const conteudoForm = {
+            textfh1: "Conheça o outro lado do debate" ,
+            textfh2: "Preencha o formulário e receba o livro!",
+            src: img,
+            value:"BAIXAR O E -BOOK"
+        }
+        return(
+            <div className="main">
+                <Section1  {...conteudoTxt} {...conteudoForm}/>
+                <Section2/>
+                <Section3/>
+                <Footer/>
+            </div>
+
+
+
+        )
+    }
+}
